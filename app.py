d074618b6e068ee86610c462670a7ed69f491c9d
kaleb.soller <kalebsoller@gmail.com>
2024-10-05 08:24:53 -0400

    feat(layout): add buttons to windows

--- app.py
+++ app.py
@@ -30,15 +30,16 @@ class AudioApp(QWidget):  # Every widget we see is a QWidget
     def __init__(self):
         super().__init__()  # Activate the QWidget Superclass
         self.settings()
+        self.initUI()
 
     # Settings
     def settings(self):
-        self.setWindowTitle("Audio Adjuster")
+        self.setWindowTitle("Audio Player")
         self.setGeometry(240, 210, 600, 300)
 
     # Design
     def initUI(self):
-        self.title = QLabel("Audio Adjuster")
+        self.title = QLabel("Volume Slider")
         self.file_list = QListWidget()
         self.btn_opener = QPushButton("Choose a file")
         self.btn_play = QPushButton("Play")
@@ -53,9 +54,32 @@ class AudioApp(QWidget):  # Every widget we see is a QWidget
         self.slider.setTickPosition(QSlider.TickPosition.TicksBelow)
         self.slider.setTickInterval(10)
 
-        
+        # Create a layout
+        # Master (Columns or rows, or just the objects)
+        self.master = QVBoxLayout()
+        row = QHBoxLayout()
+        col1 = QVBoxLayout()
+        col2 = QVBoxLayout()
+
+        self.master.addWidget(self.title)
+        self.master.addWidget(self.slider)
+
+        col1.addWidget(self.file_list)
+        col2.addWidget(self.btn_opener)
+        col2.addWidget(self.btn_play)
+        col2.addWidget(self.btn_pause)
+        col2.addWidget(self.btn_resume)
+        col2.addWidget(self.btn_reset)
+
+        row.addLayout(col1)
+        row.addLayout(col2)
+
+        self.master.addLayout(row)
+
+        self.setLayout(self.master)
 
     # Event Handler
+
 
 if __name__ == "__main__":
     app = QApplication([])
